67f2910794968ff1a585048bdb23185d90717120
Riccardo Colombo <rikyregna@gmail.com>
2020-08-26 15:23:27 +0200

    first step (one object)

--- js/script.js
+++ js/script.js
@@ -1,5 +1,32 @@
-$(function () {
+/* 
+-Creare un oggetto che descriva uno studente con le seguenti proprietà: nome, cognome ed età.
+Stampare a schermo attraverso il for in tutte le proprietà. 
+-Creare un array di oggetti di studenti.Ciclare su tutti gli studenti e stampare per ognuno nome e cognome.
+-Dare la possibilità all’ utente attraverso 3 prompt di aggiungere un nuovo oggetto studente inserendo nell’ ordine: nome, cognome e età.
+Usate prima i console.log e poi provare a stampare con jQuery.
+
+BONUS: Provate ad utilizzare Handlebars. */
 
 
 
-});
+// 1. creazione di un oggetto studente con le seguenti proprietà
+const studente = {
+	nome: 'Riccardo',
+	cognome: 'Colombo',
+	eta: 37
+}
+
+for (const key in studente) {
+	console.log(`Il valore della proprietà ${key} è  ${studente[key]}`);
+}
+
+// 2. creare un array di oggetti studenti. Ciclare gli studenti e stampare per ognuno nome e cognome 
+
+const classe16 = [{
+
+
+
+	}
+
+
+]
